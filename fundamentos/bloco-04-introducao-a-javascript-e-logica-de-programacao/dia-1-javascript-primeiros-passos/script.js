9baa228e50574c670feec880d47e30dde9d7c890
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-02 19:31:18 -0300

    Adicionado requisito 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,17 @@
+const a = 2;
+const b = 4;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+const num1 = 100;
+const num2 = 69;
+
+if (num1 > num2) {
+    console.log(num1)
+} else if (num2 > num1) {
+    console.log(num2)
+}
