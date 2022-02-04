63ab6a66b285187ee8d1a2eae75f139d326238b7
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-03 23:13:55 -0300

    Adiciona item 4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -12,7 +12,7 @@ for (let index = 0; index < numbers.length; index += 1) {
     console.log(resultado);
 }
 
-//ITEM 3
+// ITEM 3
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 for (let index = 0; index < numbers.length; index += 1) {
@@ -20,3 +20,16 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 let media = soma / numbers.length;
 console.log(media);
+
+// ITEM 4
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+}
+let media = soma / numbers.length;
+if (resultado > 20) {
+    console.log("valor maior que 20")
+} else if (resultado <= 20) {
+    console.log("valor menor ou igual a 20")
+}
