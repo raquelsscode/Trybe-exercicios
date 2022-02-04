cc77cc70ff918d836a53cee304de52c6dda5e402
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-03 23:02:53 -0300

    Adicionado item 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -3,3 +3,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index])
 }
+
+// ITEM 2
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0
+for (let index = 0; index < numbers.length; index += 1) {
+    resultado += numbers[index];
+    console.log(resultado);
+}
