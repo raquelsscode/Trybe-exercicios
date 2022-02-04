77b136ec21bfb005fb34a2e5c25d6ab4bc17fd1a
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-03 23:18:20 -0300

    Adiciona item 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -33,3 +33,15 @@ if (resultado > 20) {
 } else if (resultado <= 20) {
     console.log("valor menor ou igual a 20")
 }
+
+// ITEM 5
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeroMaior = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > numeroMaior) {
+        numeroMaior = numbers[index];
+    }
+}
+
+console.log(numeroMaior);
