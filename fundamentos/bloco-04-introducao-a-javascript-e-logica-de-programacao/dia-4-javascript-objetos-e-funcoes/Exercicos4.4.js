34f2fc2c609ac8a2156d9ef79275c590427c1a59
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-07 20:50:49 -0300

    Adicionado item 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicos4.4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicos4.4.js
@@ -3,20 +3,38 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
-console.log('Bem-vinda,' + info.personagem)
+}
+let moreinfos = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+}
+
+// console.log('Bem-vinda,' + info.personagem)
 
 //ITEM 2
 info.recorrente = 'Sim'
 
-console.log(info)
+// console.log(info)
 
-//ITEM 3
-for (let key in info) {
-    console.log(key)
-}
+// //ITEM 3
+// for (let key in info) {
+//     console.log(key)
+// }
 
-//ITEM 4
+// //ITEM 4
+// for (let key in info) {
+//     console.log(info[key]);
+// }
+
+//ITEM 5
+
 for (let key in info) {
-    console.log(info[key]);
+    console.log(info[key] + ' e ' + moreinfos[key]);
+    if (key === 'recorrente' &&
+        info[key] === 'Sim' &&
+        moreinfos[key] === 'Sim') {
+        console.log('Ambos recorrentes')
+    }
 }
