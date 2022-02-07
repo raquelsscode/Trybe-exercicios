f7a79222473ffe6c79b81cad4e18436204a50711
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-07 19:47:02 -0300

    Adicionados itens 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicos4.4.js
@@ -0,0 +1,12 @@
+// ITEM 1
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+console.log('Bem-vinda,' + info.personagem)
+
+//ITEM 2
+info.recorrente = 'Sim'
+
+console.log(info)
