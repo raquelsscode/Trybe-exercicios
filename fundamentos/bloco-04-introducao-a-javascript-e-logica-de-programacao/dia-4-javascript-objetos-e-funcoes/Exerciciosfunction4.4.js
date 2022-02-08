4ba06ad9e49bdbc2c22cbe60c215ce029dd39038
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-08 13:52:43 -0300

    Adicionado item 3 e 4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exerciciosfunction4.4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exerciciosfunction4.4.js
@@ -22,4 +22,17 @@ function indiceDoMaior(numeros) {
     return indiceMaior;
 }
 
-console.log(indiceDoMaior([2, 3, 6, 7, 10, 1]));
+console.log(indiceDoMenor([2, 3, 6, 7, 10, 1]));
+
+//ITEM 3
+function indiceDoMenor(numeros) {
+    let indiceMenor = 0;
+    for (let indice in numeros) {
+        if (numeros[indiceMenor] > numeros[indice]) {
+            indiceMenor = indice;
+        }
+    }
+    return indiceMenor;
+}
+
+console.log(indiceDoMenor([2, 4, 6, 7, 10, 0, -3]));
