9bc5f521f4054fed931c9d5ea9586eb469abce30
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-08 00:21:15 -0300

    Adicionado item 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exerciciosfunction4.4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exerciciosfunction4.4.js
@@ -1,12 +1,25 @@
-//ITEM 1
+// //ITEM 1
 
-function verificapalindromo(palindromo) {
-    let reverse = '';
-    let min = palindromo.toUpperCase()
-    reverse = palindromo.split('').reverse().join('');
+// function verificapalindromo(palindromo) {
+//     let reverse = '';
+//     let min = palindromo.toUpperCase()
+//     reverse = palindromo.split('').reverse().join('');
 
-    if (reverse === palindromo) {
-        console.log('true');
-    } else console.log('false')
+//     if (reverse === palindromo) {
+//         console.log('true');
+//     } else console.log('false')
+// }
+// verificapalindromo('palalelepipedo');
+
+//ITEM 2
+function indiceDoMaior(numeros) {
+    let indiceMaior = 0;
+    for (let indice in numeros) {
+        if (numeros[indiceMaior] < numeros[indice]) {
+            indiceMaior = indice;
+        }
+    }
+    return indiceMaior;
 }
-verificapalindromo('palalelepipedo');
+
+console.log(indiceDoMaior([2, 3, 6, 7, 10, 1]));
