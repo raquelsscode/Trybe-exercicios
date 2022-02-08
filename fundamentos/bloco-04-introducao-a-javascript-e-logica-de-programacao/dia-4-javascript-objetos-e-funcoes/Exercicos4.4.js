b44b3772ab693eb1a06bf754accd60052b21447b
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-07 21:27:05 -0300

    Descomentado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicos4.4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicos4.4.js
@@ -1,42 +1,42 @@
 // ITEM 1
-// let info = {
-//     personagem: 'Margarida',
-//     origem: 'Pato Donald',
-//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-// }
-// let moreinfos = {
-//     personagem: 'Tio Patinhas',
-//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-//     nota: 'O último MacPatinhas',
-//     recorrente: 'Sim',
-// }
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+}
+let moreinfos = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+}
 
-// console.log('Bem-vinda,' + info.personagem)
+console.log('Bem-vinda,' + info.personagem)
 
-//ITEM 2
-// info.recorrente = 'Sim'
+// ITEM 2
+info.recorrente = 'Sim'
 
-// console.log(info)
+console.log(info)
 
-// //ITEM 3
-// for (let key in info) {
-//     console.log(key)
-// }
+//ITEM 3
+for (let key in info) {
+    console.log(key)
+}
 
-// //ITEM 4
-// for (let key in info) {
-//     console.log(info[key]);
-// }
+//ITEM 4
+for (let key in info) {
+    console.log(info[key]);
+}
 
-//ITEM 5
-// for (let key in info) {
-//     console.log(info[key] + ' e ' + moreinfos[key]);
-//     if (key === 'recorrente' &&
-//         info[key] === 'Sim' &&
-//         moreinfos[key] === 'Sim') {
-//         console.log('Ambos recorrentes')
-//     }
-// }
+// ITEM 5
+for (let key in info) {
+    console.log(info[key] + ' e ' + moreinfos[key]);
+    if (key === 'recorrente' &&
+        info[key] === 'Sim' &&
+        moreinfos[key] === 'Sim') {
+        console.log('Ambos recorrentes')
+    }
+}
 
 //ITEM 6
 let leitor = {
