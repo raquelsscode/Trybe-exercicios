45df1349d6437f5c71971f361611f234d3cf7bfe
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-02-07 21:14:29 -0300

    Adicionado item 6

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicos4.4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicos4.4.js
@@ -1,20 +1,20 @@
 // ITEM 1
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-}
-let moreinfos = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-}
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// }
+// let moreinfos = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+// }
 
 // console.log('Bem-vinda,' + info.personagem)
 
 //ITEM 2
-info.recorrente = 'Sim'
+// info.recorrente = 'Sim'
 
 // console.log(info)
 
@@ -29,12 +29,26 @@ info.recorrente = 'Sim'
 // }
 
 //ITEM 5
+// for (let key in info) {
+//     console.log(info[key] + ' e ' + moreinfos[key]);
+//     if (key === 'recorrente' &&
+//         info[key] === 'Sim' &&
+//         moreinfos[key] === 'Sim') {
+//         console.log('Ambos recorrentes')
+//     }
+// }
 
-for (let key in info) {
-    console.log(info[key] + ' e ' + moreinfos[key]);
-    if (key === 'recorrente' &&
-        info[key] === 'Sim' &&
-        moreinfos[key] === 'Sim') {
-        console.log('Ambos recorrentes')
-    }
-}
+//ITEM 6
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [{
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+    }, ],
+};
+let fullName = leitor.nome + ' ' + leitor.sobrenome
+let livroFavorito = leitor.livrosFavoritos[0]
+console.log('O livro favorito de ' + fullName + ' se chama ' + livroFavorito['titulo']);
